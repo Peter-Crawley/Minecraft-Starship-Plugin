41d7cc14649f9b97efe0487f470ac2b45965fc52
trainb0y1 <66213737+trainb0y1@users.noreply.github.com>
2021-11-27 13:38:05 -0700

    Test player editable slots
    Nothing important to commit here, I just want a revert point.

--- src/main/kotlin/powerarmor/ModuleScreen.kt
+++ src/main/kotlin/powerarmor/ModuleScreen.kt
@@ -1,5 +1,6 @@
 package io.github.petercrawley.minecraftstarshipplugin.powerarmor
 
+import io.github.petercrawley.minecraftstarshipplugin.MinecraftStarshipPlugin.Companion.plugin
 import io.github.petercrawley.minecraftstarshipplugin.utils.NamedItem
 import io.github.petercrawley.minecraftstarshipplugin.utils.Screen
 import org.bukkit.Material
@@ -8,18 +9,13 @@ import org.bukkit.event.inventory.InventoryType
 import org.bukkit.inventory.ItemStack
 
 class ModuleScreen(player: Player) : Screen() {
+
 	init {
 		createScreen(player, InventoryType.CHEST, "Power Armor Modules")
-
+		playerEditableSlots.addAll(mutableSetOf(1,2))
 	}
 
-	override fun onScreenButtonClicked(slot: Int) {
-		when (slot) {
-			0 -> TODO()
-		}
-	}
-
-	override fun onPlayerPlaceItem(slot: Int, items: ItemStack) {
-		TODO()
+	override fun onPlayerChangeItem(slot: Int, oldItems: ItemStack?, newItems: ItemStack?) {
+		plugin.logger.warning("Slot $slot used to contain $oldItems, but now contains $newItems")
 	}
 }
