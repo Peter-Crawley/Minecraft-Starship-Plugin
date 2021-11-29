6af98d241a66a4cb0e178551fd0674e6f220274e
trainb0y1 <66213737+trainb0y1@users.noreply.github.com>
2021-11-28 20:34:26 -0700

    Debug messages

--- src/main/kotlin/powerarmor/ArmorActivatorRunnable.kt
+++ src/main/kotlin/powerarmor/ArmorActivatorRunnable.kt
@@ -10,16 +10,26 @@ class ArmorActivatorRunnable : BukkitRunnable() {
 		// if they're wearing a full set of power armor and have power left.
 		// Disable them otherwise
 		getServer().onlinePlayers.forEach { player ->
+			player.sendMessage("\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n")
+			player.sendMessage("Wearing: ${PowerArmorManager.isWearingPowerArmor(player)}")
+			player.sendMessage("Enabled: ${PowerArmorManager.getPowerArmorEnabled(player)}")
+			player.sendMessage("Power: ${PowerArmorManager.getArmorPower(player)}")
+			player.sendMessage("Weight: ${PowerArmorManager.getCurrentModuleWeight(player)} / ${PowerArmorManager.maxModuleWeight}")
+
 			if (PowerArmorManager.isWearingPowerArmor(player) && PowerArmorManager.getPowerArmorEnabled(player) && PowerArmorManager.getArmorPower(
 					player
 				) > 0 && PowerArmorManager.getCurrentModuleWeight(player) <= PowerArmorManager.maxModuleWeight
 			) {
+				player.sendMessage("Enabling modules:")
 				PowerArmorManager.getModules(player).forEach { module ->
 					module.enableModule(player)
+					player.sendMessage("   - ${module.name}")
 				}
 			} else {
+				player.sendMessage("Disabling modules:")
 				PowerArmorManager.getModules(player).forEach { module ->
 					module.disableModule(player)
+					player.sendMessage("   - ${module.name}")
 				}
 			}
 		}
