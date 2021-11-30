01e664f1b176b943d4ff70aa6d25547bcd269ceb
trainb0y1 <66213737+trainb0y1@users.noreply.github.com>
2021-11-30 16:37:26 -0700

    Remove debug messages (again)

--- src/main/kotlin/powerarmor/ArmorActivatorRunnable.kt
+++ src/main/kotlin/powerarmor/ArmorActivatorRunnable.kt
@@ -11,24 +11,13 @@ class ArmorActivatorRunnable : BukkitRunnable() {
 		// Disable them otherwise
 		getServer().onlinePlayers.forEach { player ->
 			val playerManager = PlayerPowerArmor(player)
-			player.sendMessage("\n ".repeat(20))
-			player.sendMessage("--------Power Armor Status--------\n ")
-			player.sendMessage("Power Items ${PowerArmorManager.powerItems}")
-			player.sendMessage("Power: ${playerManager.armorPower} / ${PowerArmorManager.maxPower}")
-			player.sendMessage("Weight: ${playerManager.moduleWeight} / ${PowerArmorManager.maxModuleWeight}")
-			player.sendMessage("Enabled: ${playerManager.armorEnabled}")
-			player.sendMessage("Wearing: ${playerManager.wearingPowerArmor}")
 			if (playerManager.wearingPowerArmor && playerManager.armorEnabled && playerManager.armorPower > 0 && playerManager.moduleWeight <= PowerArmorManager.maxModuleWeight) {
-				player.sendMessage("\nEnabling Modules:")
 				playerManager.modules.forEach { module ->
 					module.enableModule(player)
-					player.sendMessage("   - ${module.name}")
 				}
 			} else {
-				player.sendMessage("\nDisabling Modules:")
 				playerManager.modules.forEach { module ->
 					module.disableModule(player)
-					player.sendMessage("   - ${module.name}")
 				}
 			}
 		}
