4ff00cb3c3c287634e9a21902ecc9317a2d7c966
trainb0y1 <66213737+trainb0y1@users.noreply.github.com>
2021-11-29 19:30:44 -0700

    Revert "Delete hardcoded potion effect modules"
    This reverts commit df66b62cf4b3fde921cd308d4757a56915aceae0.

---
+++ src/main/kotlin/powerarmor/modules/JumpModule.kt
@@ -0,0 +1,25 @@
+package io.github.petercrawley.minecraftstarshipplugin.powerarmor.modules
+
+import io.github.petercrawley.minecraftstarshipplugin.MinecraftStarshipPlugin
+import org.bukkit.Bukkit
+import org.bukkit.Material
+import org.bukkit.NamespacedKey
+import org.bukkit.inventory.ShapedRecipe
+import org.bukkit.potion.PotionEffectType
+
+class JumpModule : EffectModule() {
+	override val name = "Jump Module"
+	override val lore = "Grants jump boost when applied"
+	override val effect: PotionEffectType = PotionEffectType.JUMP
+	override val effectMultiplier = 1
+	override val weight = 2
+
+	init {
+		createItem()
+		val recipe = ShapedRecipe(NamespacedKey(MinecraftStarshipPlugin.plugin, "power-module-jump"), item)
+		recipe.shape("ooo", "oco", "ooo")
+		recipe.setIngredient('o', Material.GOLD_INGOT)
+		recipe.setIngredient('c', Material.RABBIT_FOOT)
+		Bukkit.addRecipe(recipe)
+	}
+}
---
+++ src/main/kotlin/powerarmor/modules/NightVisionModule.kt
@@ -0,0 +1,26 @@
+package io.github.petercrawley.minecraftstarshipplugin.powerarmor.modules
+
+import io.github.petercrawley.minecraftstarshipplugin.MinecraftStarshipPlugin
+import org.bukkit.Bukkit
+import org.bukkit.Material
+import org.bukkit.NamespacedKey
+import org.bukkit.inventory.ShapedRecipe
+import org.bukkit.potion.PotionEffectType
+
+class NightVisionModule : EffectModule() {
+	override val name = "Night Vision Module"
+	override val lore = "Grants night vision boost when applied"
+	override val effect: PotionEffectType = PotionEffectType.NIGHT_VISION
+	override val effectMultiplier = 1
+	override val durationBonus = 220 // Don't want night vision to flicker.
+	override val weight = 1
+
+	init {
+		createItem()
+		val recipe = ShapedRecipe(NamespacedKey(MinecraftStarshipPlugin.plugin, "power-module-vision"), item)
+		recipe.shape("ooo", "oco", "ooo")
+		recipe.setIngredient('o', Material.GOLD_INGOT)
+		recipe.setIngredient('c', Material.SPIDER_EYE)
+		Bukkit.addRecipe(recipe)
+	}
+}
